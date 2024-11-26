9ecee1f632e5747a47e2caea625c072fa401a5eb
Patricio Avila <p.avil;af1998@gmail.com>
2024-11-26 17:54:36 -0300

    ajustando chunks

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,16 @@ export default defineConfig({
     outDir: 'dist', // El directorio de salida para los archivos construidos
     rollupOptions: {
       input: 'index.html', // Asegúrate de que Vite sepa cuál es el archivo principal de entrada
+      output: {
+        manualChunks(id) {
+          // Si el archivo se encuentra dentro de node_modules, lo agrupa en un "chunk" llamado 'vendor'
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      },
     },
+    chunkSizeWarningLimit: 1000, // Aumenta el límite de tamaño de los chunks (por defecto es 500 kB)
   },
   server: {
     open: true, // Esto abrirá el navegador automáticamente en el puerto que uses
