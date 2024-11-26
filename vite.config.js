a260ae7916eb677ed992b95b69eb809ba4fea300
Patricio Avila <p.avil;af1998@gmail.com>
2024-11-26 13:14:16 -0300

    Setting up the deployment to vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Este es el directorio de salida que Vercel tomará
+  },
   server: {
     port: 3000, 
   },
